585eb527a518a20ca6357aafb9d709f8a5b68917
janwika <jawi2030@gmail.com>
2025-11-28 11:28:26 +0100

    added out folder

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 poetry run python3 ./src/run.py \
-  --algorithm ga \
+  --algorithm aco \
   --service_time 30 \
   --places_file data/places.json \
   --travel_times_file data/travel_times.json \
-  --days 1
+  --days 2
